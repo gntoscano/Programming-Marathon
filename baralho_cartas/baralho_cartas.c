9c27386e2cec1f5c0fbf5f943a69527026c62ad5
Gabriel Toscano <toscanogabriel55@gmail.com>
2024-08-05 20:06:17 -0300

    Created a prototype for the challenge B

---
+++ baralho_cartas/baralho_cartas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N; //Numero de cartas
+    int K; //Embaralhamentos
+    int final[N];
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &final[i]);
+
+    }
+
+    return 0;
+}
